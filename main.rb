714121d3d19d3d1fa3e315616bec0750c2e753a2
IzumiSy <beetle-noise@gmx.com>
2015-11-14 21:54:37 +0900

    Add UserRoutes class and enable logging

--- main.rb
+++ main.rb
@@ -6,14 +6,24 @@ require 'rack'
 require 'mongoid'
 require 'config'
 
-class Application < Sinatra::Base
+class UserRoutes < Sinatra::Base
   helpers Sinatra::Param
 
-  post '/user/signup' do
+  get '/user' do
+    "user"
+  end
 
+  post '/user/signup' do
+    "signup"
   end
 
   post '/user/signin' do
-
+    "signin"
   end
+end
+
+class Application < Sinatra::Base
+  enable :logging
+
+  use UserRoutes
 end
