bdaea53f5bbac200852deaa1ff58b3270bb699cb
IzumiSy <beetle-noise@gmx.com>
2015-11-15 11:29:28 +0900

    Improved test

--- spec/user_routes_spec.rb
+++ spec/user_routes_spec.rb
@@ -1,22 +1,25 @@
 require_relative "./spec_helper.rb"
 
-describe "GET /user" do
-  it "should get users" do
-    get "/user"
-    expect(last_response.status).to eq(200)
-  end
-end
+describe "POST /api/user/signup" do
+  let(:signup_params) {
+    { name: "Jonathan",
+      email: "jonathan@test.com" }
+  }
 
-describe "POST /user/signup" do
-  it "should not make users sign up without params" do
+  it "should NOT make users sign up without params" do
     post "/user/signup"
     expect(last_response.status).to eq(400)
   end
+
+  it "should make users sign up with params" do
+    post "/api/user/signup", signup_params
+    expect(last_response.status).to eq(202)
+  end
 end
 
-describe "POST user/signin" do
+describe "POST /api/user/signin" do
   it "should not make users sign in without params" do
-    post "/user/signin"
+    post "/api/user/signin"
     expect(last_response.status).to eq(400)
   end
 end
