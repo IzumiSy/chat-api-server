df6dd9779dda716bc3f596380380b9ffe9078b0a
IzumiSy <beetle-noise@gmx.com>
2015-11-15 13:29:19 +0900

    Fix validation

--- models/user.rb
+++ models/user.rb
@@ -11,7 +11,7 @@ class User
 
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
 
