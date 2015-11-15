a46599ccdba39c6c33494ac9ee1b71fb73f6a78c
IzumiSy <beetle-noise@gmx.com>
2015-11-15 11:15:35 +0900

    Fix tests

--- spec/user_routes_spec.rb
+++ spec/user_routes_spec.rb
@@ -3,18 +3,21 @@ require_relative "./spec_helper.rb"
 describe "GET /user" do
   it "should get users" do
     get "/user"
+    expect(last_response.status).to eq(200)
   end
 end
 
 describe "POST /user/signup" do
-  it "should make users sign up" do
-    post "/users/signup"
+  it "should not make users sign up without params" do
+    post "/user/signup"
+    expect(last_response.status).to eq(400)
   end
 end
 
 describe "POST user/signin" do
-  it "should make users sign in" do
-    post "/users/signin"
+  it "should not make users sign in without params" do
+    post "/user/signin"
+    expect(last_response.status).to eq(400)
   end
 end
 
