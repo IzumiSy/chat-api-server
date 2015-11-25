5bb3abda8d044c8af41e69b66b12deb50f75050b
IzumiSy <beetle-noise@gmx.com>
2015-11-25 22:39:27 +0900

    Add status flag in all room, message model

--- models/message.rb
+++ models/message.rb
@@ -9,6 +9,8 @@ class Message
   belongs_to :room, index: true, counter_cache: :messages_count
 
   field :content, type: String
+  field :status,  type: Integer
+
   validates :content, presence: true
 
   after_save do |messsage|
--- models/room.rb
+++ models/room.rb
@@ -9,6 +9,7 @@ class Room
 
   field :name, type: String
   field :messages_count, type: Integer
+  field :status, type: Integer
 
   validates :name, presence: true
   validates :name, uniqueness: true
