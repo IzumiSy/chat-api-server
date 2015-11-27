d80d909402ea03b07426211d91d6ea803788d19d
IzumiSy <beetle-noise@gmx.com>
2015-11-27 23:56:50 +0900

    Add exception handling for redis

--- controllers/basic.rb
+++ controllers/basic.rb
@@ -1,11 +1,20 @@
-redis = Redis.new
 
 class BasicRoutes < Sinatra::Base
+  helpers Sinatra::Param
+
   get '/api/ping' do
     body 'pong'
   end
 
   post '/api/user/new' do
+    redis = Redis.new
+
+    begin
+      redis.ping
+    rescue Exception => e
+      print "#{e.message}\n"
+    end
+
     param :name, String, required: true
     uuid = SecureRandom.uuid
   end
