d05324ef6f389f917dff8e333ee0caccc1192d78
IzumiSy <beetle-noise@gmx.com>
2015-11-28 18:22:49 +0900

    Add empty lines

--- controllers/room.rb
+++ controllers/room.rb
@@ -5,6 +5,7 @@ class RoomRoutes < Sinatra::Base
   # TODO Error handling on validation error in creating a new room
   post '/api/room/new' do
     param :name, String, required: true
+
     room = Room.create(name: params[:name])
     body room.to_json
     status 202
@@ -12,8 +13,10 @@ class RoomRoutes < Sinatra::Base
 
   get '/api/room/:id' do
     param :id, String, required: true
+
     room_id = params[:id]
     return if room_id.empty?
+
     if Room.where(id: room_id).exists?
       room_messages = Room.find(room_id).messages
       body room_messages.to_json
