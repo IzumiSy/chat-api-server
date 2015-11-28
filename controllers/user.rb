8c6bdf2357fe60133e9670caf11be99f238c61c0
IzumiSy <beetle-noise@gmx.com>
2015-11-28 18:21:38 +0900

    Creation of user in model

--- controllers/user.rb
+++ controllers/user.rb
@@ -4,8 +4,15 @@ class UserRoutes < Sinatra::Base
 
   post '/api/user/new' do
     param :name, String, required: true
+
     client_ip = request.ip
     client_uuid = SecureRandom.uuid
     client_name = params[:name]
+
+    return if client_ip.empty? or client_name.empty?
+
+    user = User.create(name: client_name, ip: client_ip, token: client_uuid)
+    body user.to_json
+    status 202
   end
 end
