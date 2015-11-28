5bf9e484b82d5a12df7459fc4d77653fa2e4d6c7
IzumiSy <beetle-noise@gmx.com>
2015-11-28 18:19:02 +0900

    Add fields in user model

--- models/user.rb
+++ models/user.rb
@@ -9,6 +9,10 @@ class User
   has_many   :messages
 
   field :name, type: String
+  field :ip,   type: String
+  field :token, type: String
 
   validates :name, presence: true
+  validates :ip, presence: true, uniequness: true
+  validates :token, presence: true, uniqueness: true
 end
