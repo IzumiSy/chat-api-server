a09fa74d4e230a6d0759fd1ac2ec97f78a162b75
IzumiSy <beetle-noise@gmx.com>
2015-12-03 23:09:45 +0900

    Add user creating test

--- spec/user_test_spec.rb
+++ spec/user_test_spec.rb
@@ -1,7 +1,17 @@
 require_relative "./spec_helper.rb"
 
 describe "POST /api/user/new" do
+  let(:user1) { { name: "test1" } }
 
+  it "should NOT create a new user without name" do
+    post "/api/user/new"
+    expect(last_response.status).to eq(400)
+  end
+
+  it "should create a new user" do
+    post "/api/user/new", user1
+    expect(last_response.status).to eq(202)
+  end
 end
 
 describe "GET /api/user/usable" do
