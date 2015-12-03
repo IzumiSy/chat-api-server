9b6cf796b4197735d29ea7ba736497347213dca5
IzumiSy <beetle-noise@gmx.com>
2015-12-03 23:33:21 +0900

    Fix name availability test

--- controllers/user.rb
+++ controllers/user.rb
@@ -17,14 +17,14 @@ class UserRoutes < Sinatra::Base
     status 202
   end
 
-  get '/api/user/usable' do
+  get '/api/user/usable/:name' do
     param :name, String, required: true
 
     provided_name = params[:name]
     is_name_availability = User.where(name: provided_name).exists?
 
     result = {
-      status: is_name_availability
+      status: !is_name_availability
     }
     body result.to_json
     status 200
--- spec/user_test_spec.rb
+++ spec/user_test_spec.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 require_relative "./spec_helper.rb"
 
 describe "POST /api/user/new" do
@@ -16,13 +18,25 @@ end
 
 describe "GET /api/user/usable" do
   let(:user) { { name: "jonathan" } }
-  let(:user_name) { "jonathan" }
+  let(:inavailable_name) { "jonathan" }
+  let(:available_name) { "bob" }
 
-  it "should get true if the user whose name is the same already exists" do
+  before do
     post "/api/user/new", user
     expect(last_response.status).to eq(202)
+  end
 
-    get "/api/user/usable&name=#{user_name}"
+  it "should get TRUE if the user who has the same name already exists" do
+    get "/api/user/usable/#{inavailable_name}"
     expect(last_response.status).to eq(200)
+    availability_stat = JSON.parse(last_response.body)
+    expect(availability_stat["status"]).to eq(false)
+  end
+
+  it "should get FALSE if the user who has the same name doesnt exist" do
+    get "/api/user/usable/#{available_name}"
+    expect(last_response.status).to eq(200)
+    availability_stat = JSON.parse(last_response.body)
+    expect(availability_stat["status"]).to eq(true)
   end
 end
