cb1b3aa0d575e0da3b1e57c0cd1ce307379f6059
IzumiSy <beetle-noise@gmx.com>
2015-12-10 23:46:45 +0900

    Add callbacks for room model

--- models/user.rb
+++ models/user.rb
@@ -5,6 +5,9 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  after_save :handle_room_enter
+  after_destroy :handle_room_leave
+
   belongs_to :room
   has_many   :messages
 
@@ -16,4 +19,14 @@ class User
   validates :name, presence: true
   validates :ip, presence: true, uniqueness: true
   validates :token, presence: true, uniqueness: true
+
+  protected
+
+  def handle_room_enter
+    # Handles user entering to the room
+  end
+
+  def handle_room_leave
+    # Handles user leaving from the room
+  end
 end
