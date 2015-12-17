5ee1c8ba6f0f628bf0b7bd3b9bfb2f8af43218c2
IzumiSy <beetle-noise@gmx.com>
2015-12-18 00:12:10 +0900

    Add a port for admin auth

--- controllers/basic.rb
+++ controllers/basic.rb
@@ -9,5 +9,15 @@ class BasicRoutes < Sinatra::Base
   get '/api/ping' do
     body 'pong'
   end
+
+  # Get admin token to use restricted API
+  post '/api/admin/auth' do
+    param :md5_hash, String, required: true
+
+    # Checks if MD5 hash is really the same as the one in .env
+    # this port gives back an access token for some restricted API
+
+    status 200
+  end
 end
 
