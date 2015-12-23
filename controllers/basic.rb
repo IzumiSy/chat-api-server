de5e030585a0fcb7239d163b715c80bea2e4b625
IzumiSy <beetle-noise@gmx.com>
2015-12-23 16:34:00 +0900

    Change workflow of admin authorization

--- controllers/basic.rb
+++ controllers/basic.rb
@@ -20,32 +20,37 @@ class BasicRoutes < Sinatra::Base
     body 'pong'
   end
 
+  # Update is_admin flag of the given user to true
+  # in order to allow call of admin restricted API
   post '/api/admin/auth' do
     param :auth_hash, String, required: true
+    param :user_id, String, required: true
 
     admin_pass = ENV['ADMIN_PASS']
-    login_hash = params[:auth_hash]
-
     halt 500 if admin_pass.empty?
-    halt 400 if login_hash.empty?
 
-    # Checks if MD5 hash is really the same as the one in .env
-    # this port gives back an access token for some restricted API
+    login_hash = params[:auth_hash]
+    user_id = params[:user_id]
+
     password_hash = Digest::MD5.hexdigest(admin_pass)
     if password_hash == login_hash
-      now = Time.now.to_s
-      client_ip = request.ip.to_s
-      seed = "#{now}+#{client_ip}"
-
-      auth_token = Digest::MD5.new.update(seed).to_s
-      @redis.set(client_ip, auth_token)
-
-      response = { auth_token: auth_token }
-      body response.to_json
-      status 200
+      status_code, result = user_admin_promotion(user_id)
+      body result.to_json
+      status status_code
     else
      status 401
     end
+  end
+
+  protected
+
+  def user_admin_promotion(user_id)
+    return 500, {} unless User.where(id: user_id).exists?
+
+    user = User.find(user_id)
+    user.update_attribute(:is_admin, true)
+
+    return 202, user
   end
 end
 
--- spec/basic_test_spec.rb
+++ spec/basic_test_spec.rb
@@ -4,26 +4,39 @@ require 'pry'
 ENV['ADMIN_PASS'] = 'testpass'
 
 describe "POST /api/admin/auth" do
-  let(:env_hash) { ENV["ADMIN_PASS"] }
-  let(:success_hash) { Digest::MD5.hexdigest('testpass') }
-  let(:error_hash) { Digest::MD5.hexdigest('testpassee') }
   let(:redis) { Helpers.redis_connect() }
 
-  it "should get auth_token" do
-    post 'api/admin/auth', { auth_hash: success_hash }
-    expect(last_response.status).to eq(200)
+  let(:success_param) {
+    { auth_hash: Digest::MD5.hexdigest('testpass'),
+      user_id: "1234" }
+  }
+  let(:undefined_user_param) {
+    { auth_hash: Digest::MD5.hexdigest('testpass'),
+      user_id: "12345" }
+  }
+  let(:error_param) {
+    { auth_hash: Digest::MD5.hexdigest('testpassee'),
+      user_id: "2345" }
+  }
 
-    auth_token = JSON.parse(last_response.body)["auth_token"]
-    expect(redis.get("127.0.0.1")).to eq(auth_token)
+  it "should get auth_token" do
+    pending "Still not implemented"
+    post 'api/admin/auth', success_param
+    expect(last_response.status).to eq(200)
+  end
+
+  it "should NOT update an user with undefined user_id" do
+    post 'api/admin/auth', undefined_user_param
+    expect(last_response.status).to eq(500)
   end
 
   it "should NOT get auth_token with incorrect password" do
-    post 'api/admin/auth', { auth_hash: error_hash }
+    post 'api/admin/auth', error_param
     expect(last_response.status).to eq(401)
   end
 
-  it "should NOT get auth_token with empty parameter" do
-    post 'api/admin/auth', { auth_hash: "" }
+  it "should NOT get auth_token with empty parameters" do
+    post 'api/admin/auth'
     expect(last_response.status).to eq(400)
   end
 end
