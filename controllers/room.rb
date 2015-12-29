0da7a4ebd4a5d06465be551739aba3944c718d3c
IzumiSy <beetle-noise@gmx.com>
2015-12-29 21:05:51 +0900

    Model object reloading

--- controllers/room.rb
+++ controllers/room.rb
@@ -165,16 +165,16 @@ class RoomRoutes < Sinatra::Base
     return case type
       when :ENTER then
         unless is_user_exist_in_room
-          Room.increment_counter(:users_count, room_id);
+          Room.increment_counter(:users_count, room_id)
           user.update_attributes!(room_id: room_id)
         end
-        [ 202, { users_count: room.users_count }.to_json ]
+        [ 202, { users_count: room.reload.users_count }.to_json ]
       when :LEAVE then
         if is_user_exist_in_room
-          Room.decrement_counter(:users_count, room_id);
+          Room.decrement_counter(:users_count, room_id)
           user.update_attributes!(room_id: nil)
         end
-        [ 202, { users_count: room.users_count }.to_json ]
+        [ 202, { users_count: room.reload.users_count }.to_json ]
       else
         [ 500, {}.to_json ]
       end
