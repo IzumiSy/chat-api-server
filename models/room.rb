da0efaf751e6433a07bd48a5662a7fc16646c464
IzumiSy <beetle-noise@gmx.com>
2016-01-03 20:56:57 +0900

    Implement all leave option

--- models/room.rb
+++ models/room.rb
@@ -46,11 +46,15 @@ class Room
       end
   end
 
+  # If "all" is specfied to room_id parameter, this function proceeds
+  # the transaction that the specified user leaves from the current room.
   def self.room_transaction(room_id, token, type)
     user = User.find_by(token: token)
-    room = Room.only(:users, :users_count).find(room_id)
+    is_all_leave_mode = (room_id == "all" && type == :LEAVE)
+    room_id = user.room.id if is_all_leave_mode
 
-    unless room
+    room = Room.only(:users, :users_count).find(room_id)
+    if !room
       return [ 404, { status: nil }.to_json ]
     end
 
@@ -58,23 +62,33 @@ class Room
 
     return case type
       when :ENTER then
-        unless is_user_exist_in_room
-          if user.room && Room.find(user.room.id).users.find(user.id)
-            Room.decrement_counter(:users_count, user.room.id)
-          end
-          Room.increment_counter(:users_count, room_id)
-          user.update_attributes!(room_id: room_id)
-        end
+        transaction_enter(is_user_exist_in_room, room_id, user)
         [ 202, { users_count: room.reload.users_count }.to_json ]
       when :LEAVE then
-        if is_user_exist_in_room
-          Room.decrement_counter(:users_count, user.room.id)
-          user.update_attributes!(room_id: nil)
-        end
+        transaction_leave(is_user_exist_in_room, room_id, user)
         [ 202, { users_count: room.reload.users_count }.to_json ]
       else
         [ 500, {}.to_json ]
       end
+  end
+
+  protected
+
+  def self.transaction_enter(is_in_room, room_id, user)
+    unless is_in_room
+      if user.room && Room.find(user.room.id).users.find(user.id)
+        Room.decrement_counter(:users_count, user.room.id)
+      end
+      Room.increment_counter(:users_count, room_id)
+      user.update_attributes!(room_id: room_id)
+    end
+  end
+
+  def self.transaction_leave(is_in_room, room_id, user)
+    if is_in_room
+      Room.decrement_counter(:users_count, room_id)
+      user.update_attributes!(room_id: nil)
+    end
   end
 end
 
