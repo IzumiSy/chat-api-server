96283a5e046cf3716667a1cfa90bd21d488e64a1
IzumiSy <beetle-noise@gmx.com>
2016-01-06 19:41:09 +0900

    Remove unused loggings

--- services/message_service.rb
+++ services/message_service.rb
@@ -1,10 +1,8 @@
 module MessageService
-  @clients = []
   @io = Sinatra::RocketIO
 
   @io.on :connect do |client|
-    logger.info "New client connected: #{client.session}, #{client.address}"
-    @clients << client
+
   end
 
   def self.broadcastMessage(room_id, message)
