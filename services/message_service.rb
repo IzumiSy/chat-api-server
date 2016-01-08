bd2187bc8615ed34f70d6d82447f1e42c0ddb4a1
IzumiSy <beetle-noise@gmx.com>
2016-01-09 01:13:59 +0900

    Add logging

--- services/message_service.rb
+++ services/message_service.rb
@@ -2,7 +2,7 @@ module MessageService
   @io = Sinatra::RocketIO
 
   @io.on :connect do |client|
-
+    puts "[INFO] New client: #{client.session}, #{client.address}"
   end
 
   def self.broadcastMessage(room_id, message)
