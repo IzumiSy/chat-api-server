2b52c8090e4230b062c0934073d806d01d226061
IzumiSy <beetle-noise@gmx.com>
2016-01-10 11:27:41 +0900

    Add more logging of RocketIO

--- services/message_service.rb
+++ services/message_service.rb
@@ -5,6 +5,14 @@ module MessageService
     puts "[INFO] New client: #{client.session}, #{client.address}"
   end
 
+  @io.on :disconnect do |client|
+    puts "[INFO] Client diconnected: #{client.session}, #{client.address}"
+  end
+
+  @io.on :error do |client|
+    puts "[INFO] Client error: #{client.session}, #{client.address}"
+  end
+
   def self.broadcastMessage(room_id, user_id, message)
     params = { message: message, user_id: user_id }
     @io.push :newMessage, params, { channel: room_id }
