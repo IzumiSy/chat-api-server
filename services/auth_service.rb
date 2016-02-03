fbc87d4e41675010c1faabcf22d4e2cae9fb4a56
IzumiSy <beetle-noise@gmx.com>
2016-02-02 21:29:41 -0800

    Use singular class

--- services/auth_service.rb
+++ services/auth_service.rb
@@ -3,20 +3,22 @@ require_relative "./redis_service"
 module AuthService
   include RedisService
 
-  def self.is_admin?(params)
-    token = params[:token]
+  class << self
+    def is_admin?(params)
+      token = params[:token]
 
-    user = User.find_by(token: token)
-    user.read_attribute(:is_admin)
-  end
+      user = User.find_by(token: token)
+      user.read_attribute(:is_admin)
+    end
 
-  def self.is_logged_in?(params)
-    token = params[:token]
+    def is_logged_in?(params)
+      token = params[:token]
 
-    RedisService.connect(takeover: true)
-    has_session = RedisService.get(token)
-    is_user_found = User.find_by(token: params[:token])
+      RedisService.connect(takeover: true)
+      has_session = RedisService.get(token)
+      is_user_found = User.find_by(token: params[:token])
 
-    if has_session && is_user_found then true else false end
+      if has_session && is_user_found then true else false end
+    end
   end
 end
