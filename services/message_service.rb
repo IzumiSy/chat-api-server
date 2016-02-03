29f2fc8ab36ee90f1388886ab468e54856f238b6
IzumiSy <beetle-noise@gmx.com>
2016-02-02 21:30:57 -0800

    Use singular class

--- services/message_service.rb
+++ services/message_service.rb
@@ -20,57 +20,59 @@ module MessageService
     User.user_deletion(user)
   end
 
-  def self.broadcast_message(room_id, params)
-    @io.push :newMessage, params.to_json, { channel: room_id }
-  end
-
   # Pseudo enum
   module SYSTEM_LOG_TYPE
     USER_ENTER = 0
     USER_LEAVE = 1
   end
 
-  def self.broadcast_enter_msg(user, room_id)
-    broadcast_system_log(SYSTEM_LOG_TYPE::USER_ENTER, user.name, room_id)
-    broadcast_members_update(room_id)
-    broadcast_room_update()
-  end
-
-  def self.broadcast_leave_msg(user, room_id)
-    broadcast_system_log(SYSTEM_LOG_TYPE::USER_LEAVE, user.name, room_id)
-    broadcast_members_update(room_id)
-    broadcast_room_update()
-  end
-
-  private
-
-  def self.broadcast_system_log(type, user_name, room_id)
-    case type
-    when SYSTEM_LOG_TYPE::USER_ENTER then
-      # @io.push userEnter, user_name, { channel: room_id }
-    when SYSTEM_LOG_TYPE::USER_LEAVE then
-      # @io.push userLeave, user_name, { channel: room_id }
-    else
-      # TODO kinda exception handling is needed here
+  class << self
+    def broadcast_message(room_id, params)
+      @io.push :newMessage, params.to_json, { channel: room_id }
     end
-  end
 
-  def self.broadcast_room_update()
-    params = Room.all.to_json(only: Room::ROOM_DATA_LIMITS)
-    @io.push :updateRooms, params
-  end
+    def broadcast_enter_msg(user, room_id)
+      broadcast_system_log(SYSTEM_LOG_TYPE::USER_ENTER, user.name, room_id)
+      broadcast_members_update(room_id)
+      broadcast_room_update()
+    end
 
-  def self.broadcast_members_update(room_id)
-    room = Room.find(room_id)
-    users = room.users.asc(:name).to_json(only: User::USER_DATA_LIMITS)
-    @io.push :updateMembers, users, { channel: room_id }
-  end
+    def broadcast_leave_msg(user, room_id)
+      broadcast_system_log(SYSTEM_LOG_TYPE::USER_LEAVE, user.name, room_id)
+      broadcast_members_update(room_id)
+      broadcast_room_update()
+    end
 
-  def self.resolve_disconnected_users(user_id, new_session)
-    user = User.find(user_id)
-    return unless user
-    if user.session == new_session
-      User.user_deletion(user)
+    private
+
+    def broadcast_system_log(type, user_name, room_id)
+      case type
+      when SYSTEM_LOG_TYPE::USER_ENTER then
+        # @io.push userEnter, user_name, { channel: room_id }
+      when SYSTEM_LOG_TYPE::USER_LEAVE then
+        # @io.push userLeave, user_name, { channel: room_id }
+      else
+        # TODO kinda exception handling is needed here
+      end
+    end
+
+    def broadcast_room_update()
+      params = Room.all.to_json(only: Room::ROOM_DATA_LIMITS)
+      @io.push :updateRooms, params
+    end
+
+    def broadcast_members_update(room_id)
+      room = Room.find(room_id)
+      users = room.users.asc(:name).to_json(only: User::USER_DATA_LIMITS)
+      @io.push :updateMembers, users, { channel: room_id }
+    end
+
+    def resolve_disconnected_users(user_id, new_session)
+      user = User.find(user_id)
+      return unless user
+      if user.session == new_session
+        User.user_deletion(user)
+      end
     end
   end
 end
