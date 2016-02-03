ce7ee21a3571284da10b5bb795110bce812ecd4b
IzumiSy <beetle-noise@gmx.com>
2016-02-02 21:35:03 -0800

    Fix RedisService

--- services/redis_service.rb
+++ services/redis_service.rb
@@ -1,40 +1,40 @@
 module RedisService
   class << self
-    def self.connect(options)
+    def connect(options)
       is_takeover = options[:takeover]
       @redis = is_takeover ?
         @redis ? @redis : redis_new() :
         redis_new()
     end
 
-    def self.set(key, data)
+    def set(key, data)
       redis_ping(@redis)
       @redis.set(key, data)
     end
 
-    def self.get(key)
+    def get(key)
       redis_ping(@redis)
       @redis.get(key)
     end
 
-    def self.delete(key)
+    def delete(key)
       redis_ping(@redis)
       @redis.del(key)
     end
 
-    def self.instance
+    def instance
       @redis
     end
 
     protected
 
-    def self.redis_new
+    def redis_new
       redis = Redis.new(host: ENV["REDIS_IP"], port: ENV["REDIS_PORT"])
       redis_ping(redis)
       redis
     end
 
-    def self.redis_ping(redis)
+    def redis_ping(redis)
       begin
         redis.ping
       rescue Exception => e
