d82d5554956acae7aadd07ff4d0b4f33321fcdc2
IzumiSy <beetle-noise@gmx.com>
2016-03-03 00:30:12 -0800

    Fix message test spec

--- spec/message_test_spec.rb
+++ spec/message_test_spec.rb
@@ -3,7 +3,7 @@ require_relative "./spec_helper.rb"
 describe "POST /api/message/:room_id" do
   let(:user) { create(:user) }
   let(:room) { create(:room) }
-  let(:invalid_message) { { content: "Hello", token: user.token } }
+  let(:invalid_message) { { content: "Hello" } }
 
   it "should get an error when no params" do
     post "/api/message/#{room.id}"
@@ -11,7 +11,8 @@ describe "POST /api/message/:room_id" do
   end
 
   it "should get an error in posting a message to unexisted room" do
-    post "/api/message/12345", invalid_message
+    post "/api/message/12345", invalid_message,
+      { "HTTP_AUTHORIZATION" => "Basic #{user.token}" }
     expect(last_response.status).to eq(404)
   end
 end
