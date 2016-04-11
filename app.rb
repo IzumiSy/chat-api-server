fae47ee371eca289da2ff7cd3e5a36d83af0f977
IzumiSy <beetle-noise@gmx.com>
2016-04-12 00:03:18 +0900

    Disable allow_crossdomain

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ class Application < Sinatra::Base
     set :raise_errors, true
     set :show_exceptions, false
 
-    set :cometio, timeout: 20, post_interval: 1, allow_crossdomain: true
+    set :cometio, timeout: 20, post_interval: 1
     set :rocketio, websocket: false, comet: true
   end
 
