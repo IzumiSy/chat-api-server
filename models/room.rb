3686d4b1dcf08a5f04c05de11d38c6d4fbe4f008
IzumiSy <beetle-noise@gmx.com>
2016-05-02 00:45:57 +0900

    Fix

--- models/room.rb
+++ models/room.rb
@@ -73,7 +73,7 @@ class Room
 
     def transaction_enter(is_in_room, room_id, user)
       unless is_in_room
-        is_target_exist = !!User.first({ id: user.id, room_id: user.room.id })
+        is_target_exist = !!User.where(id: user.id, room_id: user.room.id).first
         if user.room && is_target_exist
           Room.decrement_counter(:users_count, user.room.id)
         end
