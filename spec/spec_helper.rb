e3ddde338c605a68db649d29c60fcaa6e06329a5
IzumiSy <beetle-noise@gmx.com>
2016-11-16 15:54:13 +0900

    Set orm type to mongoid

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+
     begin
       DatabaseCleaner.start
       FactoryGirl.lint
