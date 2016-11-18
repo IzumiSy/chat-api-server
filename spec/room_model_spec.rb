4e9de6e3667e088eb9d73638204aa31c5087e7fa
IzumiSy <beetle-noise@gmx.com>
2016-11-18 23:24:13 +0900

    Add validation specs

--- spec/room_model_spec.rb
+++ spec/room_model_spec.rb
@@ -2,4 +2,7 @@ require_relative "./spec_helper.rb"
 
 describe Room do
   it { is_expected.to have_many(:users).with_foreign_key(:room_id) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -2,4 +2,7 @@ require_relative "./spec_helper.rb"
 
 describe User do
   it { is_expected.to belong_to(:room) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
