68dfe0c4068606606218c48560279c8ae80ff116
IzumiSy <beetle-noise@gmx.com>
2016-11-18 23:30:11 +0900

    Add field specs

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -5,4 +5,9 @@ describe User do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
+
+  it { is_expected.to have_fields(:name, :face).of_type(String) }
+  it { is_expected.to have_fields(:ip, :token, :session).of_type(String) }
+  it { is_expected.to have_fields(:is_admin, :is_deleted).of_type(Mongoid::Boolean) }
+  it { is_expected.to have_field(:status).of_type(Integer) }
 end
