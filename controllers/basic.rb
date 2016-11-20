fe587174e43dce1c9b7c3f424374b40c4013923e
IzumiSy <beetle-noise@gmx.com>
2016-11-20 15:45:32 +0900

    Remove manual raise

--- controllers/basic.rb
+++ controllers/basic.rb
@@ -30,10 +30,7 @@ class BasicRoutes < RouteBase
   protected
 
   def user_admin_promotion(user_id)
-    unless user = User.find(user_id)
-      raise HTTPError::InternalServerError, "User Not Found"
-    end
-
+    user = User.find(user_id)
     user.update_attribute(:is_admin, true)
     return user.to_json(only: User::USER_DATA_LIMITS.dup << :is_admin)
   end
