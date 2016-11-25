a6eff503418ab578d758fb5baadd0d13cb436f2f
IzumiSy <beetle-noise@gmx.com>
2016-11-25 19:06:37 +0900

    Implement parse_token

--- services/auth_service.rb
+++ services/auth_service.rb
@@ -5,17 +5,22 @@ module AuthService
 
   class << self
     def is_admin?(request)
-      auth = Rack::Auth::Basic::Request.new(request.env)
-      return nil unless auth.provided?
-      return nil unless auth.basic?
-      User.is_admin_from_token(auth.params)
+      token = parse_token(request)
+      User.is_admin_from_token(token)
     end
 
     def is_logged_in?(request)
+      token = parse_token(request)
+      User.is_logged_in_from_token(token)
+    end
+
+    private
+
+    def parse_token(request)
       auth = Rack::Auth::Basic::Request.new(request.env)
       return nil unless auth.provided?
       return nil unless auth.basic?
-      User.is_logged_in_from_token(auth.params)
+      auth.params
     end
   end
 end
