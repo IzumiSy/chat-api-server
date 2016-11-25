b9934713fdf95f7bcc8b4579a3c3639ab3dc7768
IzumiSy <beetle-noise@gmx.com>
2016-11-25 20:05:40 +0900

    Implement connection checker

--- models/user.rb
+++ models/user.rb
@@ -83,6 +83,16 @@ class User
         end
     end
 
+    def setup_connection_checker(session)
+      if EM.reactor_running? && EM.reactor_thread.alive?
+        Thread.new {
+          EM.add_periodic_timer(10) do
+            user = User.find_user_by_session(session)
+          end
+        }
+      end
+    end
+
     def trigger_disconnection_resolver(client)
       Thread.new {
         if user = User.find_user_by_session(client.session)
--- services/message_service.rb
+++ services/message_service.rb
@@ -10,6 +10,7 @@ module MessageService
     puts "[INFO] New client: #{client.session}, #{client.address}"
     if user = User.find_user_by_ip(client.address)
       user.update_attributes!(session: client.session)
+      User.setup_connection_checker(client.session)
     end
   end
 
