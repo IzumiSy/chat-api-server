150abb8ab394b226ade608ebccd90fcf26a7cbfb
IzumiSy <beetle-noise@gmx.com>
2016-11-25 16:55:22 +0900

    Implement authenticate methods

--- strategies.rb
+++ strategies.rb
@@ -3,33 +3,30 @@ require 'warden'
 Warden::Strategies.add(:user) do
   def valid?
     auth = Rack::Auth::Basic::Request.new(request.env)
-    if auth.provided? and auth.basic?
-      token = auth.params
-    else
-      return false
-    end
-
-    # TODO
+    auth.provided? and auth.basic?
   end
 
   def authenticate!
-    # TODO
+    auth = Rack::Auth::Basic::Request.new(request.env)
+    RedisService.connect(takeover: true)
+    token = auth.params
+    has_session = RedisService.get(token)
+    # is_user_found = User.find_user_by_token(token)
+    if has_session then token else nil end
   end
 end
 
 Warden::Strategies.add(:admin) do
   def valid?
     auth = Rack::Auth::Basic::Request.new(request.env)
-    if auth.provided? and auth.basic?
-      token = auth.params
-    else
-      return false
-    end
-
+    auth.provided? and auth.basic?
   end
 
   def authenticate!
-
+    auth = Rack::Auth::Basic::Request.new(request.env)
+    token = auth.params
+    user = User.find_user_by_token(token)
+    user.read_attribute(:is_admin)
   end
 end
 
