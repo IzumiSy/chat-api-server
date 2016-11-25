c6f06cc869379af3471f27295e24ac0061c48492
IzumiSy <beetle-noise@gmx.com>
2016-11-25 16:21:24 +0900

    Add stretegy.rb

---
+++ strategy.rb
@@ -0,0 +1,19 @@
+require 'warden'
+
+Warden::Strategies.add(:TokenAuthrization) do
+  def valid?
+    auth = Rack::Auth::Basic::Request.new(request.env)
+    if auth.provided? and auth.basic?
+      token = auth.params
+    else
+      return false
+    end
+
+    # TODO
+  end
+
+  def authenticate!
+    # TODO
+  end
+end
+
